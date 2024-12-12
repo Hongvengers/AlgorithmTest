9db554f036dcb2e3b356dded3512ca58d17a846d
Hongvengers <rlghd3156@gmail.com>
2024-12-12 22:54:57 +0900

    fix: 프로그래머스 삼총사 재귀로도 풀어보기

--- src/main/java/edu/hong/algorithmproject/삼총사.java
+++ src/main/java/edu/hong/algorithmproject/삼총사.java
@@ -1,6 +1,25 @@
 package edu.hong.algorithmproject;
 
 public class 삼총사 {
+    public int recursion(int[] number) {
+        return calcTriple(number, 0,  0, 0);
+    }
+
+    public int calcTriple(int[] number, int index, int student, int sum) {
+        if (student == 3) {
+            return sum == 0 ? 1 : 0;
+        }
+
+        if (index == number.length) {
+            return 0;
+        }
+
+        int cur = calcTriple(number, index + 1, student + 1, sum + number[index]);
+        int next = calcTriple(number, index + 1, student, sum);
+
+        return cur + next;
+    }
+
     public int solution(int[] number) {
         // 3 ≤ number의 길이 ≤ 13
         // 조합은 (n * (n - 1) * (n - 2)) / 6 이므로
